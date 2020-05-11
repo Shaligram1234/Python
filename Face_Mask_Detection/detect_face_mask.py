6c677b54a74b3250c03e0bcddaa0c9c371729fe1
Shaligram1234 <saurabh.shaligram@gmail.com>
2020-05-11 14:46:40 +0530

    Update detect_face_mask.py

--- Face_Mask_Detection/detect_face_mask.py
+++ Face_Mask_Detection/detect_face_mask.py
@@ -37,7 +37,7 @@ while 1:
 
     # Convert image in black and white
     (thresh, black_and_white) = cv2.threshold(gray, bw_threshold, 255, cv2.THRESH_BINARY)
-    cv2.imshow('black_and_white', black_and_white)
+    #cv2.imshow('black_and_white', black_and_white)
 
     # detect face
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
@@ -62,22 +62,22 @@ while 1:
             # Detect lips counters
             mouth_rects = mouth_cascade.detectMultiScale(gray, 1.5, 5)
 
-            # Face detected but Lips not detected which means person is wearing mask
-            if(len(mouth_rects) == 0):
-                cv2.putText(img, weared_mask, org, font, font_scale, weared_mask_font_color, thickness, cv2.LINE_AA)
-            else:
-                for (mx, my, mw, mh) in mouth_rects:
+        # Face detected but Lips not detected which means person is wearing mask
+        if(len(mouth_rects) == 0):
+            cv2.putText(img, weared_mask, org, font, font_scale, weared_mask_font_color, thickness, cv2.LINE_AA)
+        else:
+            for (mx, my, mw, mh) in mouth_rects:
 
-                    if(y < my < y + h):
-                        # Face and Lips are detected but lips coordinates are within face cordinates which means lips prediction is true and
-                        # person is not waring mask
-                        cv2.putText(img, not_weared_mask, org, font, font_scale, not_weared_mask_font_color, thickness, cv2.LINE_AA)
-                        #y = int(y - 0.15 * h)
-                        cv2.rectangle(img, (mx, my), (mx + mh, my + mw), (0, 0, 255), 3)
-                        break
+                if(y < my < y + h):
+                    # Face and Lips are detected but lips coordinates are within face cordinates which `means lips prediction is true and
+                    # person is not waring mask
+                    cv2.putText(img, not_weared_mask, org, font, font_scale, not_weared_mask_font_color, thickness, cv2.LINE_AA)
+
+                    #cv2.rectangle(img, (mx, my), (mx + mh, my + mw), (0, 0, 255), 3)
+                    break
 
     # Show frame with results
-    cv2.imshow('img', img)
+    cv2.imshow('Mask Detection', img)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
